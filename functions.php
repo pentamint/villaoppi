5678f5a2be6907dd1adf363779349c0ff611b7dd
pentamint <web@pentamint.com>
2018-11-27 20:14:39 +0900

    breadcrumbs, shop page updated

--- functions.php
+++ functions.php
@@ -1,4 +1,19 @@
 <?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+
+// END ENQUEUE PARENT ACTION
+
 /**
  * Change number of products that are displayed per page (shop page)
  */
@@ -10,4 +25,10 @@ function new_loop_shop_per_page( $cols ) {
   $cols = 12;
   return $cols;
 }
-?>
+
+// enqueue css and js files
+function add_theme_scripts () {
+	wp_enqueue_style( 'normalize' , get_stylesheet_directory_uri() . '/css/normalize.min.css' , array() , '8.0.0' , 'all' );
+  wp_enqueue_script('pm-breadcrumbs', get_stylesheet_directory_uri().'/js/pm-breadcrumbs.js', array('jquery'),filemtime(get_stylesheet_directory() . '/js/pm-breadcrumbs.js' ));
+}
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
---
+++ js/pm-breadcrumbs.js
@@ -0,0 +1,8 @@
+( function ( $ ) {
+
+  $(document).ready(function() {
+    /* append breadcrumbs to parent */
+    $('#main-header').after($('.woocommerce-breadcrumb'));
+  });
+
+} )( jQuery );
